756474f344ad6a558a93763f1f6eb6329accd287
winkey62650 <winkey62650@github.com>
2026-04-17 09:07:36 +0800

    Pause site with maintenance page

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,12 @@
-const CACHE_NAME = 'poker-clock-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// 安装时缓存资源
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
-// 激活时清理旧缓存
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// 拦截请求，优先使用缓存
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request).then(response => {
-          if (!response || response.status !== 200 || response.type === 'error') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        });
-      })
-      .catch(() => caches.match('./index.html'))
+    caches.keys()
+      .then(cacheNames => Promise.all(cacheNames.map(cacheName => caches.delete(cacheName))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
